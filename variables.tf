a3d1beb00e9e1084e598b6a03ff3a5c0e408acc1
sreenivasaganesh3 <150334102+sreenivasaganesh3@users.noreply.github.com>
2024-03-30 13:36:16 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "rsgname" {
+  type = string
+}
+#azure-functions-test-rg3003
+variable "location" {
+  type = string
+}
+#West Eurpoe
+variable "stgacnt" {
+  type = string
+}
+#functionsapptestsa
+
+variable "serviceplan" {
+  type = string
+}
+#azure-functions-test-service-plan3003
+
+
+variable "laname" {
+  type = string
+}
+#test-azure-functions3003
+
+
+
+
+
+
